43234ff9a1e13972385955dbe30eafc4653dab18
CitroenGames <hallomallo2003@gmail.com>
2024-01-02 17:04:43 +0100

    clean up and changes
    now one of the struct has a actual name for it
    cleaned up names and made function clearer what the do
    removed function related to printing position
    increased gravity for dino

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,13 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
         //.add_plugins(RapierDebugRenderPlugin::default())
-        .add_systems(Startup, (setup_graphics, setup_physics))
-        .add_systems(Update, print_ball_altitude)
+        .add_systems(Startup, (setup_scene, setup_player))
+        .add_systems(Update, player_input)
         .run();
 }
 
 #[derive(Component)]
-struct Jump {
-    pub velocity: f32,
+struct Player {
     pub can_jump: bool,
 }
 
@@ -22,18 +21,18 @@ struct Score {
     pub value: u32,
 }
 
-fn setup_graphics(mut commands: Commands) {
+fn setup_scene(mut commands: Commands, mut rapier_config: ResMut<RapierConfiguration>) {
     // Add a camera so we can see the debug-render.
     commands.spawn(Camera2dBundle::default());
+
+    // Create the ground
+    commands
+    .spawn(Collider::cuboid(1000.0, 50.0))
+    .insert(TransformBundle::from(Transform::from_xyz(0.0, -200.0, 0.0)));
 }
 
-fn setup_physics(mut commands: Commands, asset_server: Res<AssetServer>) {
-    /* Create the ground. */
-    commands
-        .spawn(Collider::cuboid(1000.0, 50.0))
-        .insert(TransformBundle::from(Transform::from_xyz(0.0, -200.0, 0.0)));
-
-    /* Create the bouncing ball. */
+fn setup_player(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // Create the dino
     commands
         .spawn(RigidBody::Dynamic)
         .insert(Collider::cuboid(10.0, 12.5))
@@ -42,12 +41,11 @@ fn setup_physics(mut commands: Commands, asset_server: Res<AssetServer>) {
             texture: asset_server.load("dino.png"),
             transform: Transform::from_xyz(-500., 0., 500.).with_scale(Vec3::splat(2.)),
             ..default()
-        });
+        })
+        .insert(GravityScale(60.0));
 }
 
-fn print_ball_altitude(positions: Query<&Transform, With<RigidBody>>) {
-    for transform in positions.iter() {
-        println!("Ball altitude: {}", transform.translation.y);
- 
-    }
+fn player_input(mut velocities: Query<&mut Velocity>,
+    mut keyboard_input: ResMut<Input<KeyCode>>){
+        // here we will check if we pressed space and if we did we will add a velocity to the player
 }
