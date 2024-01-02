c82fd2b8a2aa8b84f7369b1968f1f486efa93379
CitroenGames <hallomallo2003@gmail.com>
2024-01-02 14:15:05 +0100

    using actual collision system
    using rappier for 2d collisions in stead of modifying transform

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,14 @@
-
 use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(
-            ImagePlugin::default_nearest(),
-        ))
-        .add_systems(Startup, setup)
-        .add_systems(Update, sprite_movement)
+        .add_plugins(DefaultPlugins)
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
+        //.add_plugins(RapierDebugRenderPlugin::default())
+        .add_systems(Startup, (setup_graphics, setup_physics))
+        .add_systems(Update, print_ball_altitude)
         .run();
-}
-
-#[derive(Component, Resource)]
-enum Direction {
-    Up,
-    Down,
 }
 
 #[derive(Component)]
@@ -28,42 +22,32 @@ struct Score {
     pub value: u32,
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup_graphics(mut commands: Commands) {
+    // Add a camera so we can see the debug-render.
     commands.spawn(Camera2dBundle::default());
-    commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load("dino.png"),
-            transform: Transform::from_xyz(-500., 0., 500.).with_scale(Vec3::splat(5.)),
-            ..default()
-        },
-        Direction::Up,
-
-    )).insert(Jump { velocity: 0.0, can_jump: true});
 }
 
-fn sprite_movement(
-    keyboard_input: Res<Input<KeyCode>>,
-    time: Res<Time>,
-    mut query: Query<(&mut Jump, &mut Transform)>,
-) {
-    for (mut jump, mut transform) in query.iter_mut() {
-        if keyboard_input.pressed(KeyCode::Space) && jump.can_jump {
-            // Apply jump force
-            jump.velocity = 600.0;
-            jump.can_jump = false;
-        }
+fn setup_physics(mut commands: Commands, asset_server: Res<AssetServer>) {
+    /* Create the ground. */
+    commands
+        .spawn(Collider::cuboid(1000.0, 50.0))
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, -200.0, 0.0)));
 
-        // Apply gravity
-        jump.velocity -= 1200.0 * time.delta_seconds();
+    /* Create the bouncing ball. */
+    commands
+        .spawn(RigidBody::Dynamic)
+        .insert(Collider::cuboid(10.0, 12.5))
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, 400.0, 0.0)))
+        .insert(SpriteBundle {
+            texture: asset_server.load("dino.png"),
+            transform: Transform::from_xyz(-500., 0., 500.).with_scale(Vec3::splat(2.)),
+            ..default()
+        });
+}
 
-        // Move sprite
-        transform.translation.y += jump.velocity * time.delta_seconds();
-
-        // Stop falling at ground level
-        if transform.translation.y < -300.0 {
-            transform.translation.y = -300.0;
-            jump.velocity = 0.0;
-            jump.can_jump = true;
-        }
+fn print_ball_altitude(positions: Query<&Transform, With<RigidBody>>) {
+    for transform in positions.iter() {
+        println!("Ball altitude: {}", transform.translation.y);
+ 
     }
 }
